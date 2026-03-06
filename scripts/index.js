5a6706af8bca45e1fb016c1b6f811b0fbab5f866
WiZzard <you@example.com>
2026-03-07 04:19:07 +0600

    nav responsible and login access

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", function () {
+    const username = document.getElementById("usernameInput").value;
+    const password = document.getElementById("passwordInput").value;
+
+    if (username === "admin" && password === "admin123") {
+        document.getElementById("loginSection").classList.add("hidden");
+        document.getElementById("mainSection").classList.remove("hidden");
+    } else {
+        alert("Wrong username or password");
+    }
+});
